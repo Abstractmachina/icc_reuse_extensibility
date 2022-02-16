df0a17de3d6ad6dc0fd5df5fc814b31b10ec0ed9
tc1421 <tc1421@ic.ac.uk>
2022-02-16 00:00:56 +0000

    set up superclass for template method.

---
+++ src/test/java/ic/doc/templatemethod/Sequencer.java
@@ -0,0 +1,42 @@
+package ic.doc.templatemethod;
+
+import java.util.Iterator;
+
+public abstract class Sequencer implements Iterable<Integer> {
+
+  public int term(int i) {
+    if (i < 0) {
+      throw new IllegalArgumentException("Not defined for indices < 0");
+    }
+    if (i < 2) {
+      return 1;
+    }
+    return sequence_term();
+  }
+
+  protected abstract int sequence_term();
+
+  public Iterator<Integer> iterator() {
+    return new SequenceIterator();
+  }
+
+  private class SequenceIterator implements Iterator<Integer> {
+
+    private int index = 0;
+
+    @Override
+    public boolean hasNext() {
+      return true;
+    }
+
+    @Override
+    public Integer next() {
+      return term(index++);
+    }
+
+    @Override
+    public void remove() {
+      throw new UnsupportedOperationException("remove is not implemented");
+    }
+  }
+}
